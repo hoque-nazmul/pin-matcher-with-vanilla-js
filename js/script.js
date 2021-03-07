0b0bb486da58043688c7ed41c45bc50346718b0e
nazmul98 <coder.nazmul@gmail.com>
2021-03-07 16:26:47 -0500

    added functionalities of pin generated by clicking

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 const generatedPin = document.getElementById('generate-pin');
 const generateBtn = document.getElementById('generate-btn');
+const numbers = document.getElementsByClassName('number');
+const userPinField = document.getElementById('user-pin');
+let pin = '';
 
-generateBtn.addEventListener('click', () => { 
+generateBtn.addEventListener('click', () => {
     const pin = Math.round(Math.random() * (9999 - 1000) + 1000);
     generatedPin.value = pin;
-})
+});
+
+[...numbers].forEach(num => {
+    num.addEventListener('click', () => {
+        pin += num.innerHTML;
+        userPinField.value = pin;
+    });
+});
